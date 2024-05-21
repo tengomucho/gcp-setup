a15d1ab59ce101d3dda63733313cb2d9cb3bfb79
Alvaro Moran <alvaro.moran@huggingface.co>
2024-05-21 10:22:41 +0200

    chore: update torch to 2.3.0

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ source ~/Dev/venv/hf/bin/activate
 pip install -U pip
 
 # Install torch/TPU
-pip install "torch~=2.2.0" "torch_xla[tpu]~=2.2.0" -f https://storage.googleapis.com/libtpu-releases/index.html
+pip install "torch~=2.3.0" "torch_xla[tpu]~=2.3.0" -f https://storage.googleapis.com/libtpu-releases/index.html
 
 pip install transformers[sentencepiece] accelerate
 
