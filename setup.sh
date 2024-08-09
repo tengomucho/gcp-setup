8a379f85c23d70b7c58aec1e38c5f6904b32c3b0
Alvaro Moran <alvaro.moran@huggingface.co>
2024-08-09 13:43:29 +0200

    feat: use virtualenv, update torch/torch_xla to v2.4.0

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install -y python3.10-venv
+sudo apt install -y python3-virtualenv python-is-python3
 
 git config --global credential.helper store
 
 cd ~
-python -m venv ~/Dev/venv/hf
+virtualenv ~/Dev/venv/hf
 source ~/Dev/venv/hf/bin/activate
 
 pip install -U pip
 
 # Install torch/TPU
-pip install "torch~=2.3.0" "torch_xla[tpu]~=2.3.0" -f https://storage.googleapis.com/libtpu-releases/index.html
+pip install "torch~=2.4.0" "torch_xla[tpu]~=2.4.0" -f https://storage.googleapis.com/libtpu-releases/index.html
 
 pip install transformers[sentencepiece] accelerate
 
