06a71e2564a2eb772af10b805cffd326c539970e
Alvaro Moran <alvaro.moran@huggingface.co>
2024-12-03 12:20:29 +0100

    chore: update torch to 2.5.1

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ source ~/Dev/venv/hf/bin/activate
 pip install -U pip
 
 # Install torch/TPU
-pip install "torch==2.4.0" "torchvision==2.4.0" --index-url https://download.pytorch.org/whl/cpu
-pip install "torch_xla[tpu]~=2.4.0" -f https://storage.googleapis.com/libtpu-releases/index.html
+pip install "torch==2.5.1" "torchvision==2.5.1" --index-url https://download.pytorch.org/whl/cpu
+pip install "torch_xla[tpu]~=2.5.1" -f https://storage.googleapis.com/libtpu-releases/index.html
 
 pip install transformers[sentencepiece] accelerate
 
