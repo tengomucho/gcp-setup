0bba156edac4c0554130f97eb648e3c6a43161a5
Alvaro Moran <alvaro.moran@huggingface.co>
2025-11-10 16:26:55 -0500

    fix: remove main.py added by mistake

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ source ~/Dev/venv/hf/bin/activate
 pip install -U pip
 
 # Install torch/TPU
-pip install "torch==2.5.1" "torchvision==2.5.1" --index-url https://download.pytorch.org/whl/cpu
-pip install "torch_xla[tpu]~=2.5.1" -f https://storage.googleapis.com/libtpu-releases/index.html
+pip install "torch==2.8.0" "torchvision==0.24.0" --index-url https://download.pytorch.org/whl/cpu
+pip install "torch_xla[tpu]~=2.8.0" -f https://storage.googleapis.com/libtpu-releases/index.html
 
 pip install transformers[sentencepiece] accelerate
 
