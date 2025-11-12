057cee36a31d71a1c73dcae5099d008f7607d5b1
Alvaro Moran <alvaro.moran@huggingface.co>
2025-11-12 17:21:34 +0100

    chore: update torch to 2.9.0

--- setup.sh
+++ setup.sh
@@ -6,14 +6,15 @@ sudo apt install -y python3-virtualenv python-is-python3
 git config --global credential.helper store
 
 cd ~
+rm -rf ~/Dev/venv/hf
 virtualenv ~/Dev/venv/hf
 source ~/Dev/venv/hf/bin/activate
 
 pip install -U pip
 
 # Install torch/TPU
-pip install "torch==2.8.0" "torchvision==0.24.0" --index-url https://download.pytorch.org/whl/cpu
-pip install "torch_xla[tpu]~=2.8.0" -f https://storage.googleapis.com/libtpu-releases/index.html
+pip install "torch==2.9.0" "torchvision==0.24.0" --index-url https://download.pytorch.org/whl/cpu
+pip install "torch_xla[tpu]~=2.9.0" -f https://storage.googleapis.com/libtpu-releases/index.html
 
 pip install transformers[sentencepiece] accelerate
 
