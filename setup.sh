efed72b26b25bb953a2d6fecfcb20c20a8746846
Alvaro Moran <alvaro.moran@huggingface.co>
2025-11-17 11:57:27 +0100

    fix: correct torch/torch xla installation
    Last working version is 2.8.0 for now.

--- setup.sh
+++ setup.sh
@@ -13,8 +13,9 @@ source ~/Dev/venv/hf/bin/activate
 pip install -U pip
 
 # Install torch/TPU
-pip install "torch==2.9.0" "torchvision==0.24.0" --index-url https://download.pytorch.org/whl/cpu
-pip install "torch_xla[tpu]~=2.9.0" -f https://storage.googleapis.com/libtpu-releases/index.html
+pip install "torch==2.8.0" "torchvision==0.23.0" --index-url https://download.pytorch.org/whl/cpu
+pip install scipy
+pip install --pre torch_xla[pallas] --index-url https://us-python.pkg.dev/ml-oss-artifacts-published/jax/simple/ --find-links https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 pip install transformers[sentencepiece] accelerate
 
