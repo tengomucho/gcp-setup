6fd27b8659fa073b97d52a174093d1ce6adc0fa5
Alvaro Moran <alvaro.moran@huggingface.co>
2026-05-04 11:06:38 +0200

    chore: remove pip install from basic setup
    These days I use uv

--- setup.sh
+++ setup.sh
@@ -7,17 +7,6 @@ git config --global credential.helper store
 
 cd ~
 rm -rf ~/Dev/venv/hf
-virtualenv ~/Dev/venv/hf
-source ~/Dev/venv/hf/bin/activate
-
-pip install -U pip
-
-# Install torch/TPU
-pip install "torch==2.8.0" "torchvision==0.23.0" --index-url https://download.pytorch.org/whl/cpu
-pip install scipy
-pip install --pre torch_xla[pallas] --index-url https://us-python.pkg.dev/ml-oss-artifacts-published/jax/simple/ --find-links https://storage.googleapis.com/jax-releases/libtpu_releases.html
-
-pip install transformers[sentencepiece] accelerate
 
 # Add user to docker group
 sudo usermod -aG docker $USER
